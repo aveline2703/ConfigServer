00362c99ea0dda58e6e2190c71402606c5066f74
Pratik <pratik.pj48@gmail.com>
2023-01-13 15:12:57 +0530

    create config server

---
+++ src/test/java/com/pratik/www/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratik.www.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
